cfac4fbca0c54acc1d792e1bc034538a9e4717de
Mistborntt <1297350998@qq.com>
2024-07-17 09:54:32 +0800

    240717

--- ObjectOrientedAdvance/src/InheritanceLearning/TestDemo5.java
+++ ObjectOrientedAdvance/src/InheritanceLearning/TestDemo5.java
@@ -4,6 +4,9 @@ public class TestDemo5 {
     public static void main(String[] args) {
         Student s = new Student();
         s.lunch();
+
+        InternationalStudent is = new InternationalStudent();
+        is.lunch();
     }
 }
 
@@ -21,5 +24,24 @@ class Student extends Person {
     public void lunch() {
         eat();
         drink();
+    }
+}
+
+// 留学生
+class InternationalStudent extends Person {
+    public void lunch() {
+        this.eat();
+        this.drink();
+
+        super.eat();
+        super.drink();
+    }
+
+    public void eat() {
+        System.out.println("吃意大利面");
+    }
+
+    public void drink() {
+        System.out.println("喝高端矿泉水");
     }
 }
