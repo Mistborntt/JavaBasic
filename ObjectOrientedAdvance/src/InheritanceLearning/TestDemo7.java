be3d91a03ca71ee0b83f3c334accf49a3b9576be
Mistborntt <1297350998@qq.com>
2024-07-17 14:46:56 +0800

    Update

--- ObjectOrientedAdvance/src/InheritanceLearning/TestDemo7.java
+++ ObjectOrientedAdvance/src/InheritanceLearning/TestDemo7.java
@@ -3,8 +3,7 @@ package InheritanceLearning;
 public class TestDemo7 {
     public static void main(String[] args) {
         Worker w = new Worker("ljt", 23);
-        System.out.println(w.getName());
-        System.out.println(w.getAge());
+        System.out.println(w.getName() + ", " + w.getAge());
         System.out.println(w.getClass());
     }
 }
