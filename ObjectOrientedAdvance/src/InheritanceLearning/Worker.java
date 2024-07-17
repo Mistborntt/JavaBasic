de8af05b786c20c3d2a78de29c87bab1d163582c
Mistborntt <1297350998@qq.com>
2024-07-17 14:39:46 +0800

    Update

--- ObjectOrientedAdvance/src/InheritanceLearning/Worker.java
+++ ObjectOrientedAdvance/src/InheritanceLearning/Worker.java
@@ -1,2 +1,13 @@
-package InheritanceLearning;public class Worker {
+package InheritanceLearning;
+
+public class Worker extends Human {
+    // 子类的无参构造
+    public Worker() {
+        // 隐藏super()去访问父类的无参构造
+        super();
+    }
+
+    public Worker(String name, int age) {
+        super(name, age);
+    }
 }
