89dbe184706f2f40121fdb976e8246e6663766bd
Mistborntt <1297350998@qq.com>
2024-07-17 15:10:57 +0800

    Update

--- ObjectOrientedAdvance/src/InheritanceLearning/TestDemo7.java
+++ ObjectOrientedAdvance/src/InheritanceLearning/TestDemo7.java
@@ -5,5 +5,10 @@ public class TestDemo7 {
         Worker w = new Worker("ljt", 23);
         System.out.println(w.getName() + ", " + w.getAge());
         System.out.println(w.getClass());
+
+        // 可以把对象在内存中的结构打印出来
+//        ClassLayout layout = ClassLayout.parseInstance(w);
+//        System.out.println(layout.toPrintable());
+
     }
 }
