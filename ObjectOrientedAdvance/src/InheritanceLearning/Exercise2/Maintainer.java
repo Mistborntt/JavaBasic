4050e4487e86168edb0f109130bd28588115f80f
Mistborntt <1297350998@qq.com>
2024-07-18 10:19:39 +0800

    Update

---
+++ ObjectOrientedAdvance/src/InheritanceLearning/Exercise2/Maintainer.java
@@ -0,0 +1,11 @@
+package InheritanceLearning.Exercise2;
+
+public class Maintainer extends AdminStaff{
+    public Maintainer() {
+
+    }
+
+    public Maintainer(String workNo, String name, String work) {
+        super(workNo, name, work);
+    }
+}
---
+++ ObjectOrientedAdvance/src/InheritanceLearning/Exercise2/Tutor.java
@@ -0,0 +1,11 @@
+package InheritanceLearning.Exercise2;
+
+public class Tutor extends Teacher {
+    public Tutor() {
+
+    }
+
+    public Tutor(String workNo, String name, String work) {
+        super(workNo, name, work);
+    }
+}
