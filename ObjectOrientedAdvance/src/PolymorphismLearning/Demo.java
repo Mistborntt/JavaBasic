96f021e0c0ff1972d78726dd422eb74c32005a3a
Mistborntt <1297350998@qq.com>
2024-07-18 13:52:01 +0800

    Update

--- ObjectOrientedAdvance/src/PolymorphismLearning/Demo.java
+++ ObjectOrientedAdvance/src/PolymorphismLearning/Demo.java
@@ -2,6 +2,26 @@ package PolymorphismLearning;
 
 public class Demo {
     public static void main(String[] args) {
+        // 创建三个对象，并调用register方法
+        Student s = new Student();
+        s.setName("李锦涛");
+        s.setAge(23);
 
+        Teacher t = new Teacher();
+        t.setName("朱来富");
+        t.setAge(44);
+
+        Administrator admin = new Administrator();
+        admin.setName("超级管理员");
+        admin.setAge(100);
+
+        register(s);
+        register(t);
+        register(admin);
+
+    }
+
+    public static void register(Person p) {
+        p.show();
     }
 }
---
+++ ObjectOrientedAdvance/src/PolymorphismLearning/Teacher.java
@@ -0,0 +1,8 @@
+package PolymorphismLearning;
+
+public class Teacher extends Person {
+    @Override
+    public void show() {
+        System.out.println("老师的信息为：" + getName() + "， " + getAge());
+    }
+}
