12a3b337a1995611dc3e89e9259340e7fa9b6fb6
Mistborntt <1297350998@qq.com>
2024-07-18 11:57:21 +0800

    Update

---
+++ ObjectOrientedAdvance/src/InheritanceLearning/Exercise2/Test.java
@@ -0,0 +1,7 @@
+package InheritanceLearning.Exercise2;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ObjectOrientedAdvance/src/PolymorphismLearning/Demo.java
@@ -0,0 +1,7 @@
+package PolymorphismLearning;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
